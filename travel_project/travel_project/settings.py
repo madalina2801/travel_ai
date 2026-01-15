ddd6fa8c31d602ed72ef5ef2f51e95f58bc42c7f
madalina2801 <61309140+madalina2801@users.noreply.github.com>
2026-01-15 16:34:08 +0200

    change #5

--- travel_project/travel_project/settings.py
+++ travel_project/travel_project/settings.py
@@ -73,7 +73,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
